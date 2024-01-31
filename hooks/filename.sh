250411e46dc0cad2c68ef244c280c20b0d6de2bc
vijaysek <vijaysek@cisco.com>
2024-01-31 14:02:56 +0530

    amg

--- hooks/filename.sh
+++ hooks/filename.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Specify the pattern that filenames should match.
-# This pattern checks for files that start with "file" and end with ".txt"
+# This pattern checks for files that start with "devx" and end with ".py"
 pattern="^devx.*\.py$"
+
+# Specify the file to exclude
+exclude=".pre-commit-config.yaml"
 
 # Get a list of all files that have been added or modified.
 files=$(git diff --cached --name-only --diff-filter=AM)
@@ -12,6 +15,12 @@ for file in $files
 do
     # Get the base name of the file (the part of the name after the last '/').
     base=$(basename "$file")
+
+    # If the base name of the file is the excluded file, skip this iteration
+    if [[ $base == $exclude ]]
+    then
+        continue
+    fi
 
     # If the base name of the file does not match the pattern
     if [[ ! $base =~ $pattern ]]
